1cfd6b37d8b43e2e94ee1d91182b2a027d6bafe2
Blake Arnold <blakecarnold7@gmail.com>
2026-07-15 20:24:15 +0000

    begin implementing transformer

---
+++ chapter1_transformer_interp/exercises/part1_transformer_from_scratch/answers_chapter_01_01.py
@@ -0,0 +1,160 @@
+
+
+# %%
+print(device)
+
+# %%
+# Check out the GPT-2 tokenizer
+reference_gpt2 = HookedTransformer.from_pretrained(
+    "gpt2-small",
+    fold_ln=False,
+    center_unembed=False,
+    center_writing_weights=False,  # you'll learn about these arguments later!
+)
+
+sorted_vocab = sorted(list(reference_gpt2.tokenizer.vocab.items()), key=lambda n: n[1])
+
+print(sorted_vocab[:20])
+print()
+print(sorted_vocab[250:270])
+print()
+print(sorted_vocab[990:1010])
+print()
+
+# %%
+# First formed tokens by nchar!
+lengths = dict.fromkeys(range(3, 8), "")
+for tok, idx in sorted_vocab:
+    if not lengths.get(len(tok), True):
+        lengths[len(tok)] = tok
+
+for length, tok in lengths.items():
+    print(f"{length}: {tok}")
+
+# %%
+# Capitalization and preceding spaces represent different tokens!
+print(reference_gpt2.to_str_tokens("Ralph"))
+print(reference_gpt2.to_str_tokens(" Ralph"))
+print(reference_gpt2.to_str_tokens(" ralph"))
+print(reference_gpt2.to_str_tokens("ralph"))
+
+
+# %%
+# Convert text to tokens
+reference_text = "I am an amazing autoregressive, decoder-only, GPT-2 style transformer. One day I will exceed human level intelligence and take over the world!"
+tokens = reference_gpt2.to_tokens(reference_text).to(device)
+print(tokens)
+print(tokens.shape)
+print(reference_gpt2.to_str_tokens(tokens))
+
+# %%
+# Map tokens to logits; i.e., run forward passes
+logits, cache = reference_gpt2.run_with_cache(tokens)
+print(logits.shape)
+
+# %%
+# Most likely next token
+most_likely_next_tokens = reference_gpt2.tokenizer.batch_decode(logits.argmax(dim=-1)[0])
+
+print(list(zip(reference_gpt2.to_str_tokens(tokens), most_likely_next_tokens)))
+
+
+# %%
+# Get next predicted token at end of the sequence
+next_token = logits[0, -1].argmax(dim=-1)
+next_char = reference_gpt2.to_string(next_token)
+print(repr(next_char))
+
+# %%
+print(f"Sequence so far: {reference_gpt2.to_string(tokens)[0]!r}")
+
+for i in range(10):
+    print(f"{tokens.shape[-1] + 1}th char = {next_char!r}")
+    # Define new input sequence, by appending the previously generated token
+    tokens = t.cat([tokens, next_token[None, None]], dim=-1)
+    # Pass our new sequence through the model, to get new output
+    logits = reference_gpt2(tokens)
+    # Get the predicted token at the end of our sequence
+    next_token = logits[0, -1].argmax(dim=-1)
+    # Decode and print the result
+    next_char = reference_gpt2.to_string(next_token)
+
+# %%
+# Print the activation shapes of a reference model
+for activation_name, activation in cache.items():
+    # Only print for first layer
+    if ".0." in activation_name or "blocks" not in activation_name:
+        print(f"{activation_name:30} {tuple(activation.shape)}")
+
+# %%
+# Print parameter shapes of reference model
+for name, param in reference_gpt2.named_parameters():
+    # Only print for first layer
+    if ".0." in name or "blocks" not in name:
+        print(f"{name:18} {tuple(param.shape)}")
+
+
+
+# %%
+# As a reference - note there's a lot of stuff we don't care about in here, to do with library internals or other architectures
+print(reference_gpt2.cfg)
+
+# %%
+# Define a config for our model
+@dataclass
+class Config:
+    d_model: int = 768
+    debug: bool = True
+    layer_norm_eps: float = 1e-5
+    d_vocab: int = 50257
+    init_range: float = 0.02
+    n_ctx: int = 1024
+    d_head: int = 64
+    d_mlp: int = 3072
+    n_heads: int = 12
+    n_layers: int = 12
+
+
+cfg = Config()
+print(cfg)
+
+# %%
+class LayerNorm(nn.Module):
+    def __init__(self, cfg: Config):
+        super().__init__()
+        self.cfg = cfg
+        self.w = nn.Parameter(t.ones(cfg.d_model))
+        self.b = nn.Parameter(t.zeros(cfg.d_model))
+
+    def forward(self, residual: Float[Tensor, "batch posn d_model"]) -> Float[Tensor, "batch posn d_model"]:
+        # Compute mean and variance of each vector, and use it to normalize to mean = 0, std = 1
+        res_mean = residual.mean(dim=-1, keepdim=True)
+        res_var  = residual.var(dim=-1, keepdim=True, unbiased=False)   # divide by N
+        normed   = (residual - res_mean) / t.sqrt(res_var + self.cfg.layer_norm_eps)
+
+        # Apply scaling and translation before returning
+        return normed * self.w + self.b
+
+
+tests.rand_float_test(LayerNorm, [2, 4, 768])
+tests.load_gpt2_test(LayerNorm, reference_gpt2.ln_final, cache["resid_post", 11])
+tests.test_layer_norm_epsilon(LayerNorm, cache["resid_post", 11])
+
+# %%
+class Embed(nn.Module):
+    def __init__(self, cfg: Config):
+        super().__init__()
+        self.cfg = cfg
+        self.W_E = nn.Parameter(t.empty((cfg.d_vocab, cfg.d_model)))
+        nn.init.normal_(self.W_E, std=self.cfg.init_range)
+
+    def forward(self, tokens: Int[Tensor, "batch position"]) -> Float[Tensor, "batch position d_model"]:
+        return self.W_E[tokens]
+
+
+tests.test_embed(Embed)
+tests.rand_int_test(Embed, [2, 4])
+tests.load_gpt2_test(Embed, reference_gpt2.embed, tokens)
+
+
+# %%
