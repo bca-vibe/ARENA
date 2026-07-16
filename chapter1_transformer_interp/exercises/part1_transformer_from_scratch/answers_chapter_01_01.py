ab8d2337282b409d6394734919297800df3a7a49
Blake Arnold <blakecarnold7@gmail.com>
2026-07-16 03:54:38 +0000

    train a transformer

--- chapter1_transformer_interp/exercises/part1_transformer_from_scratch/answers_chapter_01_01.py
+++ chapter1_transformer_interp/exercises/part1_transformer_from_scratch/answers_chapter_01_01.py
@@ -478,3 +478,101 @@ print(first_batch.keys())
 print(first_batch["tokens"].shape)
 
 # %%
+# Create a training loop in a class
+class TransformerTrainer:
+    def __init__(self, args: TransformerTrainingArgs, model: DemoTransformer):
+        super().__init__()
+        self.model = model
+        self.args = args
+        self.sampler = solutions.TransformerSampler(self.model, reference_gpt2.tokenizer)
+        self.optimizer = t.optim.AdamW(self.model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+        self.step = 0
+
+        self.train_loader = DataLoader(
+            dataset_dict["train"],
+            batch_size=args.batch_size,
+            shuffle=True,
+            #num_workers=4, runs faster, but kills the kernel if interrupted
+            #pin_memory=False,
+        )
+        self.test_loader = DataLoader(
+            dataset_dict["test"],
+            batch_size=args.batch_size,
+            shuffle=False,
+            #num_workers=4,
+            #pin_memory=False,
+        )
+
+    def training_step(self, batch: dict[str, Int[Tensor, "batch seq"]]) -> Float[Tensor, ""]:
+        """
+        Calculates the loss on the tokens in the batch, performs a gradient update step, and logs the loss.
+
+        Remember that `batch` is a dictionary with the single key 'tokens'.
+        """
+        tokens = batch["tokens"].to(device)
+        logits = self.model(tokens)
+        loss = -get_log_probs(logits, tokens).mean()
+        loss.backward()
+        
+        self.optimizer.step()
+        self.optimizer.zero_grad()
+        self.step += 1
+
+        wandb.log({"train_loss": loss}, step=self.step)
+        
+        return loss
+
+    @t.inference_mode()
+    def evaluate(self) -> float:
+        """
+        Evaluate the model on the test set and return the accuracy.
+        """
+        self.model.eval()
+        total_correct, total_samples = 0, 0
+
+        for batch in tqdm(self.test_loader, desc="Evaluating"):
+            tokens = batch["tokens"].to(device)
+            logits: Tensor = self.model(tokens)[:, :-1]
+            predicted_tokens = logits.argmax(dim=-1)
+            total_correct += (predicted_tokens == tokens[:, 1:]).sum().item()
+            total_samples += tokens.size(0) * (tokens.size(1) - 1)
+
+        accuracy = total_correct / total_samples
+        wandb.log({"accuracy": accuracy}, step=self.step)
+        self.model.train()
+        self.model.train()
+        return accuracy
+
+    def train(self):
+        """
+        Trains the model, for `self.args.epochs` epochs. Also handles wandb initialisation, and early stopping
+        for each epoch at `self.args.max_steps_per_epoch` steps.
+        """
+        wandb.init(project=self.args.wandb_project, name=self.args.wandb_name, config=self.args)
+        accuracy = np.nan
+
+        progress_bar = tqdm(total=self.args.max_steps_per_epoch * self.args.epochs)
+
+        print(self.sampler.sample(self.args.eval_prompt, max_tokens_generated=50))
+        for epoch in range(self.args.epochs):
+            for i, batch in enumerate(self.train_loader):
+                loss = self.training_step(batch)
+                progress_bar.update()
+                progress_bar.set_description(f"Epoch {epoch + 1}, loss: {loss:.3f}, accuracy: {accuracy:.3f}")
+                if i >= self.args.max_steps_per_epoch:
+                    break
+
+            accuracy = self.evaluate()
+            print(self.sampler.sample(self.args.eval_prompt, max_tokens_generated=50))
+
+        wandb.finish()
+
+
+# See the full run here: https://api.wandb.ai/links/dquarel/nrxuwnv7
+model = DemoTransformer(model_cfg).to(device)
+args = TransformerTrainingArgs()
+trainer = TransformerTrainer(args, model)
+trainer.train()
+
+
+# %%
