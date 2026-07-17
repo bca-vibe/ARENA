fd929994e21897780a6cc5f0ab96c9d066426a58
Blake Arnold <blakecarnold7@gmail.com>
2026-07-17 03:41:28 +0000

    start of interpretability

---
+++ chapter1_transformer_interp/exercises/part2_intro_to_mech_interp/answers_chapter_1_2.py
@@ -0,0 +1,224 @@
+# %%
+import math
+import functools
+import sys
+from pathlib import Path
+from typing import Callable
+
+import circuitsvis as cv
+import einops
+import numpy as np
+import torch as t
+import torch.nn as nn
+from eindex import eindex
+from IPython.display import display
+from jaxtyping import Float, Int
+from torch import Tensor
+from tqdm import tqdm
+from transformer_lens import (
+    ActivationCache,
+    FactoredMatrix,
+    HookedTransformer,
+    HookedTransformerConfig,
+    utils,
+)
+from transformer_lens.hook_points import HookPoint
+
+device = t.device("mps" if t.backends.mps.is_available() else "cuda" if t.cuda.is_available() else "cpu")
+
+# Make sure exercises are in the path
+chapter = "chapter1_transformer_interp"
+section = "part2_intro_to_mech_interp"
+root_dir = next(p for p in Path.cwd().parents if (p / chapter).exists())
+exercises_dir = root_dir / chapter / "exercises"
+section_dir = exercises_dir / section
+if str(exercises_dir) not in sys.path:
+    sys.path.append(str(exercises_dir))
+
+import part2_intro_to_mech_interp.tests as tests
+from plotly_utils import (
+    hist,
+    imshow,
+    plot_comp_scores,
+    plot_logit_attribution,
+    plot_loss_difference,
+)
+
+# Saves computation time, since we don't need it for the contents of this notebook
+t.set_grad_enabled(False)
+
+MAIN = __name__ == "__main__"
+
+# %%
+print(device)
+# %%
+gpt2_small: HookedTransformer = HookedTransformer.from_pretrained("gpt2-small")
+
+# %%
+# Check out GPT-2
+print(f"n_layers = {gpt2_small.cfg.n_layers}")
+print(f"n_heads = {gpt2_small.cfg.n_heads}")
+print(f"n_ctx = {gpt2_small.cfg.n_ctx}")
+# %%
+model_description_text = """## Loading Models
+
+HookedTransformer comes loaded with >40 open source GPT-style models. You can load any of them in with `HookedTransformer.from_pretrained(MODEL_NAME)`. Each model is loaded into the consistent HookedTransformer architecture, designed to be clean, consistent and interpretability-friendly.
+
+For this demo notebook we'll look at GPT-2 Small, an 80M parameter model. To try the model out, let's find the loss on this paragraph!"""
+
+loss = gpt2_small(model_description_text, return_type="loss")
+print("Model loss:", loss)
+# %%
+print(gpt2_small.to_str_tokens("gpt2"))
+print(gpt2_small.to_str_tokens(["gpt2", "gpt2"]))
+print(gpt2_small.to_tokens("gpt2"))
+print(gpt2_small.to_string([50256, 70, 457, 17]))
+
+print(gpt2_small.to_str_tokens("The Westgate is an exquisitely high end chain of fancy schmancy hotels!"))
+
+# %%
+gpt2_small("Hello world!")
+maika_text = "The Westgate is an exquisitely high end chain of fancy schmancy hotels!"
+gpt2_small(maika_text)
+
+logits: Tensor = gpt2_small(maika_text, return_type="logits")
+prediction = logits.argmax(dim=-1).squeeze()[:-1]
+print(prediction)
+
+gpt2_small.to_string(prediction)
+# %%
+logits: Tensor = gpt2_small(model_description_text, return_type="logits")
+prediction = logits.argmax(dim=-1).squeeze()[:-1]
+actual = gpt2_small.to_tokens(model_description_text).squeeze()[1:]
+is_correct = prediction == actual
+
+print(f"Model accuracy: {is_correct.sum()}/{len(actual)}")
+print(f"Correct tokens: {gpt2_small.to_str_tokens(prediction[is_correct])}")
+
+
+# %%
+gpt2_text = "Natural language processing tasks, such as question answering, machine translation, reading comprehension, and summarization, are typically approached with supervised learning on task-specific datasets."
+gpt2_tokens = gpt2_small.to_tokens(gpt2_text)
+gpt2_logits, gpt2_cache = gpt2_small.run_with_cache(gpt2_tokens, remove_batch_dim=True)
+
+print(type(gpt2_logits), type(gpt2_cache))
+
+# %%
+attn_patterns_from_shorthand = gpt2_cache["pattern", 0]
+attn_patterns_from_full_name = gpt2_cache["blocks.0.attn.hook_pattern"]
+
+t.testing.assert_close(attn_patterns_from_shorthand, attn_patterns_from_full_name)
+
+
+# %%
+layer0_pattern_from_cache = gpt2_cache["pattern", 0]
+
+q, k = gpt2_cache["q", 0], gpt2_cache["k", 0]
+seq, n_head, d_head = q.shape
+layer0_attn_scores = einops.einsum(q, k, "seqQ n h, seqK n h -> n seqQ seqK")
+mask = t.triu(t.ones((seq, seq), dtype=t.bool), diagonal=1).to(device)
+layer0_attn_scores.masked_fill_(mask, -1e9)
+layer0_pattern_from_q_and_k = (layer0_attn_scores / math.sqrt(d_head)).softmax(-1)
+t.testing.assert_close(layer0_pattern_from_cache, layer0_pattern_from_q_and_k)
+print("Tests passed!")
+
+# %%
+print(type(gpt2_cache))
+attention_pattern = gpt2_cache["pattern", 0]
+print(attention_pattern.shape)
+gpt2_str_tokens = gpt2_small.to_str_tokens(gpt2_text)
+
+print("Layer 0 Head Attention Patterns:")
+display(
+    cv.attention.attention_patterns(
+        tokens=gpt2_str_tokens,
+        attention=attention_pattern,
+        attention_head_names=[f"L0H{i}" for i in range(12)],
+    )
+)
+# L0H7 looks like a previous token head!
+# %%
+neuron_activations_for_all_layers = t.stack([
+    gpt2_cache["post", layer] for layer in range(gpt2_small.cfg.n_layers)
+], dim=1)
+# shape = (seq_pos, layers, neurons)
+
+cv.activations.text_neuron_activations(
+    tokens=gpt2_str_tokens,
+    activations=neuron_activations_for_all_layers
+)
+
+# %%
+cfg = HookedTransformerConfig(
+    d_model=768,
+    d_head=64,
+    n_heads=12,
+    n_layers=2,
+    n_ctx=2048,
+    d_vocab=50278,
+    attention_dir="causal",
+    attn_only=True,  # defaults to False
+    tokenizer_name="EleutherAI/gpt-neox-20b",
+    seed=398,
+    use_attn_result=True,
+    normalization_type=None,  # defaults to "LN", i.e. layernorm with weights & biases
+    positional_embedding_type="shortformer",
+)
+
+# %%
+from huggingface_hub import hf_hub_download
+
+REPO_ID = "callummcdougall/attn_only_2L_half"
+FILENAME = "attn_only_2L_half.pth"
+
+weights_path = hf_hub_download(repo_id=REPO_ID, filename=FILENAME)
+
+
+# %%
+model = HookedTransformer(cfg)
+pretrained_weights = t.load(weights_path, map_location=device, weights_only=True)
+model.load_state_dict(pretrained_weights)
+
+# %%
+text = "We think that powerful, significantly superhuman machine intelligence is more likely than not to be created this century. If current machine learning techniques were scaled up to this level, we think they would by default produce systems that are deceptive or manipulative, and that no solid plans are known for how to avoid this."
+
+logits, cache = model.run_with_cache(text, remove_batch_dim=True)
+
+# %%
+str_tokens = model.to_str_tokens(text)
+for layer in range(model.cfg.n_layers):
+    # Capture the attention scores
+    attention_pattern = cache["pattern", layer]
+    # Display them
+    display(cv.attention.attention_patterns(tokens=str_tokens, attention=attention_pattern))
+# Visualize them 
+
+# %% 
+cache["pattern", 0].shape
+cache["pattern", 0]
+# %%
+def current_attn_detector(cache: ActivationCache) -> list[str]:
+    """
+    Returns a list e.g. ["0.2", "1.4", "1.9"] of "layer.head" which you judge to be current-token heads
+    """
+    # Head where the average score given to the same token is >= 0.8
+    
+
+print("Heads attending to current token  = ", ", ".join(current_attn_detector(cache)))
+# %%
+def prev_attn_detector(cache: ActivationCache) -> list[str]:
+    """
+    Returns a list e.g. ["0.2", "1.4", "1.9"] of "layer.head" which you judge to be prev-token heads
+    """
+    raise NotImplementedError()
+
+
+def first_attn_detector(cache: ActivationCache) -> list[str]:
+    """
+    Returns a list e.g. ["0.2", "1.4", "1.9"] of "layer.head" which you judge to be first-token heads
+    """
+    raise NotImplementedError()
+
+
+print("Heads attending to previous token = ", ", ".join(prev_attn_detector(cache)))
+print("Heads attending to first token    = ", ", ".join(first_attn_detector(cache)))
